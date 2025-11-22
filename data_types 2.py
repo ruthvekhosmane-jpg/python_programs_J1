4da36dde6613ba652f771e8b140a7f932498024d
ruthvekhosmane-jpg <ruthvekhosmane@gmail.com>
2025-11-22 09:45:10 +0530

    Add files via upload

---
+++ data_types 2.py
@@ -0,0 +1,17 @@
+#data types -in python 3 data types
+#1.int(integer)
+#2.str(string)
+#3.float
+name="ruthvek"
+age=16
+salary=1000.00
+data="1234" 
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(salary)
+print(type(salary))
+print(data)
+print(type(data))
+
---
+++ test1.py
@@ -0,0 +1,5 @@
+print("welcome to profile building session")
+a=10
+b=10
+result=a+b
+print("adittion of a,b is:",result)
